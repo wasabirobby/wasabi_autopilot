8a1b5d67384c715c51d7b5bcc01c8de05ab9798a
WasabiRobby <79250053+WasabiRobby@users.noreply.github.com>
2021-07-25 04:52:58 -0700

    Added keymapping

--- config.lua
+++ config.lua
@@ -1,9 +1,15 @@
 Config = {}
 --Provided free on github: https://github.com/WasabiRobby
-Config.NormalSpeed = 25.0 --Normal Speed
-Config.NormalDrive = 786603 --Normal Drive Style
-Config.CrazySpeed = 100.0 --Crazy Speed
-Config.CrazyDrive = 1074528293 --Crazy Drive Style
+
+Config.NormalSpeed = 25.0 -- Normal Speed
+Config.NormalDrive = 786603 -- Normal Drive Style
+Config.CrazySpeed = 100.0 -- Crazy Speed
+Config.CrazyDrive = 1074528293 -- Crazy Drive Style
+
+Config.NormalKeybind = true -- Enable Keymapping for toggling normal driving autopilot
+Config.DefaultKey = "i" -- Default keybind to toggle normal driving autopilot
+Config.CrazyKeybind = true -- Enable Keymapping for toggling crazy driving autopilot
+Config.CrazyKey = "o" -- Default keybind to toggle crazy driving autopilot
 
 Config.StartCommand = "autopilot" -- Command to activate Auto-Pilot
 Config.NormalCMD = "normal" -- Normal driving command argument
@@ -18,9 +24,10 @@ Config.pNotify = false --If you use pNotify
 
 
 Config.Translate = {
-    [1] = "Auto Pilot - Activated",
+    [1] = "Autopilot - Activated",
     [2] = "You have not set a waypoint!",
     [3] = "You are not in a vehicle!",
-    [4] = "Auto Pilot - Disabled",
+    [4] = "Autopilot - Disabled",
+    [5] = "Autopilot Normal - Button Pressed",
+    [6] = "Autopilot Aggressive - Button Pressed",
 }
-
