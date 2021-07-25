7e6b19caa0c578d8b317eb94b392c5c38c123c8d
WasabiRobby <79250053+WasabiRobby@users.noreply.github.com>
2021-07-25 04:52:15 -0700

    Added keymapping

--- client.lua
+++ client.lua
@@ -3,6 +3,66 @@ local Speed = Config.NormalSpeed
 local Drive = Config.NormalDrive
 local mode = "normal"
 
+if Config.NormalKeybind then
+    RegisterCommand('+normalpilot', function()
+        local player = PlayerPedId()
+        if IsPedInAnyVehicle(player) then
+            mode = "normal"
+            Drive = Config.NormalDrive
+        end
+        if DoesBlipExist(GetFirstBlipInfoId(8)) and IsPedInAnyVehicle(player) then
+            local blip = GetFirstBlipInfoId(8)
+            local bCoords = GetBlipCoords(blip)
+            ClearPedTasks(player)
+            local veh = GetVehiclePedIsIn(player, false)
+            TaskVehicleDriveToCoord(player, veh, bCoords, tonumber(Speed), 0, veh, Drive, 0, true)
+            SetDriveTaskDrivingStyle(player, Drive)
+            if Config.MythicNotify then
+                exports['mythic_notify']:SendAlert('success', Config.Translate[5], 2500)
+            elseif Config.pNotify then
+                exports['pNotify']:SendNotification({text = Config.Translate[5], type = "success", 2500})
+            end
+        end
+    end, false)
+
+    RegisterCommand('-normalpilot', function()
+        if IsPedInAnyVehicle(PlayerPedId()) then
+            ClearPedTasks(PlayerPedId())
+        end
+    end, false)
+    RegisterKeyMapping('+normalpilot', 'Autopilot(Normal Driving)', 'keyboard', Config.DefaultKey)
+end
+
+if Config.CrazyKeybind then
+    RegisterCommand('+crazypilot', function()
+        local player = PlayerPedId()
+        if IsPedInAnyVehicle(player) then
+            mode = "crazy"
+            Drive = Config.CrazyDrive
+        end
+        if DoesBlipExist(GetFirstBlipInfoId(8)) and IsPedInAnyVehicle(player) then
+            local blip = GetFirstBlipInfoId(8)
+            local bCoords = GetBlipCoords(blip)
+            ClearPedTasks(player)
+            local veh = GetVehiclePedIsIn(player, false)
+            TaskVehicleDriveToCoord(player, veh, bCoords, tonumber(Speed), 0, veh, Drive, 0, true)
+            SetDriveTaskDrivingStyle(player, Drive)
+            if Config.MythicNotify then
+                exports['mythic_notify']:SendAlert('success', Config.Translate[6], 2500)
+            elseif Config.pNotify then
+                exports['pNotify']:SendNotification({text = Config.Translate[6], type = "success", 2500})
+            end
+        end
+    end, false)
+
+    RegisterCommand('-crazypilot', function()
+        if IsPedInAnyVehicle(PlayerPedId()) then
+            ClearPedTasks(PlayerPedId())
+        end
+    end, false)
+    RegisterKeyMapping('+crazypilot', 'Autopilot(Agressive Driving)', 'keyboard', Config.DefaultKey)
+end
+
 RegisterCommand(Config.StartCommand, function(source, args)
     local player = PlayerPedId()
 
@@ -36,13 +96,13 @@ RegisterCommand(Config.StartCommand, function(source, args)
         
         elseif Config.pNotify then
             exports['pNotify']:SendNotification({text = Config.Translate[2], type = "error", timeout = 2500})
-        else
+        elseif Config.MythicNotify
             exports['mythic_notify']:SendAlert('error', Config.Translate[2], 2500)
         end
     
     elseif Config.pNotify then
         exports['pNotify']:SendNotification({text = Config.Translate[3], type = "error", timeout = 2500})
-    else
+    elseif Config.MythicNotify then
         exports['mythic_notify']:SendAlert('error', Config.Translate[3], 2500)
     end
 
@@ -63,4 +123,3 @@ RegisterCommand(Config.StopCommand, function(source, args)
     end
 
 end, false)
-
